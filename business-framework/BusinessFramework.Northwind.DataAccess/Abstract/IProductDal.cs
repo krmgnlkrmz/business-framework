ad084fba31b763f57992d399f29e669fd79c3197
kerem gönülkirmaz <kerem_gonulkirmaz@hotmail.com>
2019-02-13 22:57:38 +0200

    Sample class added

---
+++ business-framework/BusinessFramework.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessFramework.Core.DataAccess;
+using BusinessFramework.Northwind.Entities.Concrete;
+
+namespace BusinessFramework.Northwind.DataAccess.Abstract
+{
+    interface IProductDal:IEntityRepository<Product>
+    {
+    }
+}
---
+++ business-framework/BusinessFramework.Northwind.Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessFramework.Core.Entities;
+
+namespace BusinessFramework.Northwind.Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
