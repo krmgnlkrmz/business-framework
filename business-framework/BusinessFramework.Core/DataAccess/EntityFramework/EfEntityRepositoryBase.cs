b3062a01ccc77f1bb3fb1b1fedc62546dfc637dc
kerem gönülkirmaz <kerem_gonulkirmaz@hotmail.com>
2019-02-13 22:09:11 +0200

    IQueryableRepository & EntityFramework added

---
+++ business-framework/BusinessFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessFramework.Core.Entities;
+
+namespace BusinessFramework.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity> where TEntity:class,IEntity,new() where TContext:DbContext,new()
+    {
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
