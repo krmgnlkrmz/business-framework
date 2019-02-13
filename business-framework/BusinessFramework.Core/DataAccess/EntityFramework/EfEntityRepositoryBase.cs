f6fceb03149a9cf602c8ac571591bd593b22df5d
kerem gönülkirmaz <kerem_gonulkirmaz@hotmail.com>
2019-02-13 22:35:28 +0200

    EntityFramework implementation added

--- business-framework/BusinessFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ business-framework/BusinessFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -13,27 +13,50 @@ namespace BusinessFramework.Core.DataAccess.EntityFramework
     {
         public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>() .Where(filter).ToList();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
         }
 
         public TEntity Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
         }
 
         public TEntity Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
     }
 }
